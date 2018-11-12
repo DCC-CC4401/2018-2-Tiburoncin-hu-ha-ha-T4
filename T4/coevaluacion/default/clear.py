9e2c4ba8884aba38a393028f7f0e8e072bb335e3
Francisco95 <fjmunoz95@gmail.com>
2018-11-12 19:54:45 -0300

    added script for default erase of the database, creating the migrations all again

---
+++ T4/coevaluacion/default/clear.py
@@ -0,0 +1,21 @@
+import os
+
+'''
+Function that erase all the content of the database and create the migrations again.
+Run this file from terminal (not manage.py shell) while you are on ./T4 folder, run:
+
+python3 coevaluacion/default/clear.py
+
+
+'''
+
+
+# reset the data base
+def erase_database():
+    os.system('rm -r ./coevaluacion/migrations/*')
+    os.system('rm ./db.sqlite3')
+    os.system('python3 manage.py makemigrations coevaluacion')
+    os.system('python3 manage.py migrate')
+
+
+erase_database()
